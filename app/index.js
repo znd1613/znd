caadae572ae4d14315392969d7ea3a67695754a1
yuwentao199 <381001035@qq.com>
2016-11-18 19:51:57 +0800

    创建赞拿度的远程仓库

---
+++ app/index.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+	document.documentElement.style.fontSize=innerWidth/10+"px";
+	window.onresize=function(){
+		document.documentElement.style.fontSize=innerWidth/10+"px";
+		// 测试git
+	}
