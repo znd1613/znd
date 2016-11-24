e5c8f42e65a6cd6ef3b5251854dcf67471f870c5
WeiHongqiang <707657436@qq.com>
2016-11-24 09:19:28 +0800

    whq

--- whq/header/self-adaption.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-	document.documentElement.style.fontSize=innerWidth/10+"px";
-	window.onresize=function(){
-		document.documentElement.style.fontSize=innerWidth/10+"px";
-		// 测试git
-	}
-}
