c7e8d13e1a4c0ed6ea325bea0fdca6932624fcee
WeiHongqiang <707657436@qq.com>
2016-11-24 09:17:03 +0800

    whq

--- app/index.js
+++ app/index.js
@@ -1,6 +1,47 @@
-window.onload=function(){
-	document.documentElement.style.fontSize=innerWidth/10+"px";
-	window.onresize=function(){
-		document.documentElement.style.fontSize=innerWidth/10+"px";
-		// 测试git
-	}
+
+	angular.module('projectDemo',['ui.router','angularCSS',
+		'projectDemo.homePage',
+		'projectDemo.second_frame'])	
+		.config(function($stateProvider,$urlRouterProvider){
+
+
+		 $urlRouterProvider.otherwise('/home')
+
+
+		  $stateProvider
+		  .state({
+		    name:'register',
+		    url:'/register',
+		    css:'app/models/freeWalker/register.css',
+		    templateUrl:'app/models/freeWalker/register.html'
+		    // controller:'homeCtrl'
+		  })
+		 
+		})
+		// 点击切换侧边栏隐藏或者出现
+		.controller("ctr1",function($scope,$location){
+			$scope.istext1=false;
+			$scope.istext2=false;
+			$scope.istext3=true;
+			$scope.lsClick = function(){
+				if ($scope.istext3==true) {
+					$scope.istext3=false;
+					$scope.istext1=false;
+					$scope.istext2=true;
+				}
+				else{
+					$scope.istext3=true;
+					$scope.istext1=true;
+					$scope.istext2=false;
+				}
+				
+   			}
+   			// 点击头部左侧图标，统一回到首页部分
+   			$scope.backClick=function(){
+   				$location.url('/home');
+   			}
+
+		})
+
+
+
---
+++ app/self-adaption.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+	document.documentElement.style.fontSize=innerWidth/10+"px";
+	window.onresize=function(){
+		document.documentElement.style.fontSize=innerWidth/10+"px";
+		// 测试git
+	}
+}
